fec68e99b848252de9f7bf1abfe684123e0e1c74
GibsonCS <gibson.cruzds@gmail.com>
2025-04-07 17:53:41 -0300

    feat: add e2t test

---
+++ e2e-coverage/src/server.js
@@ -0,0 +1,41 @@
+const http = require('node:http')
+const { once } = require('node:events')
+
+const DEFAULT_CREDENTIALS = {
+    login: 'gibson',
+    password: '123'
+}
+
+const routes = {
+    '/api/login:get': (request, response) => {
+        response.writeHead(200)
+        return response.end()
+    },
+
+    '/api/login:post': async (request, response) => {
+       const user = JSON.parse(await once(request, "data"))
+       if(user.username !== DEFAULT_CREDENTIALS.login || user.password !== DEFAULT_CREDENTIALS.password) {
+        response.writeHead(401)
+        return response.end('unauthorized!')
+       }
+    },
+
+    default(request, response){
+        response.writeHead(404)
+        return response.end('not found!')
+    }
+}
+
+const handler = (request, response) => {
+    const {url, method} = request
+    const routeKey = `${url.toLowerCase()}:${method.toLowerCase()}`
+    const chosen = routes[routeKey] || routes.default
+    return chosen(request, response)
+}
+
+const server = http.createServer(handler)
+    .listen(3000, () => console.log('Server is running at 3000'))
+
+module.exports = server
+
+
---
+++ e2e-coverage/test/server.test.js
@@ -0,0 +1,47 @@
+const {describe, it, after, before} = require('mocha')
+const supertest = require('supertest')
+const assert = require('assert')
+
+describe('API Suite test', () => {
+    let server
+    before((done) => {
+        server = require("../src/server");
+        server.once('listening', done)
+    })
+
+    after(done => server.close(done))
+
+   describe('/api/login:get', () => {
+    it('Should return HTTP status 200', async() => {
+        const response = (await supertest(server)
+        .get('/api/login')
+        .expect(200))
+
+    assert.strictEqual(response.status, 200)
+    })
+   })
+
+   describe('/api/not-found', () => {
+    it('Should return HTTP status 404 and message not found!', async () => {
+        const response = await supertest(server)
+        .get('/api/not-found').expect(404)
+
+    assert.strictEqual(response.status, 404)
+    assert.deepStrictEqual(response.text, 'not found!')
+    })
+   })
+
+   describe('/api/login:post', () => {
+    it('Should return HTTP status 401 and unauthorized! if user or password is not valid', async () => {
+        const response = await supertest(server)
+        .post('/api/login')
+        .send({username: "gibson", password: "123s"})
+        .expect(401)
+
+    assert.deepStrictEqual(response.text, 'unauthorized!')
+    })
+   })
+})
+
+
+
